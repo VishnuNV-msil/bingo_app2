5ebf6eb57072b016702b20d3d74c2907cd37025d
VishnuNV-msil <vishnu.vishwanath@marketsimplified.com>
2023-02-21 10:47:20 +0530

    gridline length calculation error fixed

--- lib/Helpers/GridLines.dart
+++ lib/Helpers/GridLines.dart
@@ -10,81 +10,79 @@ class GridLines extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     double cellWidth = size.width / 5;
     double cellHeight = size.height / 5;
-    double lineLength = size.width * 5;
+    double lineLength = size.width;
     double offsetX;
     double offsetY;
 
     if (bingoList.contains('c1')) {
       offsetX = cellWidth / 2;
-      drawLineVerticalLine(canvas, offsetX);
+      drawLineVerticalLine(canvas, offsetX, lineLength);
     }
     if (bingoList.contains('c2')) {
       offsetX = cellWidth * 1.5;
-      drawLineVerticalLine(canvas, offsetX);
+      drawLineVerticalLine(canvas, offsetX, lineLength);
     }
     if (bingoList.contains('c3')) {
       offsetX = cellWidth * 2.5;
-      drawLineVerticalLine(canvas, offsetX);
+      drawLineVerticalLine(canvas, offsetX, lineLength);
     }
     if (bingoList.contains('c4')) {
       offsetX = cellWidth * 3.5;
-      drawLineVerticalLine(canvas, offsetX);
+      drawLineVerticalLine(canvas, offsetX, lineLength);
     }
     if (bingoList.contains('c5')) {
       offsetX = cellWidth * 4.5;
-      drawLineVerticalLine(canvas, offsetX);
+      drawLineVerticalLine(canvas, offsetX, lineLength);
     }
 
     if (bingoList.contains('r1')) {
       offsetY = cellHeight / 2;
-      drawHorizontalLine(canvas, offsetY);
+      drawHorizontalLine(canvas, offsetY, lineLength);
     }
     if (bingoList.contains('r2')) {
       offsetY = cellHeight * 1.5;
-      drawHorizontalLine(canvas, offsetY);
+      drawHorizontalLine(canvas, offsetY, lineLength);
     }
     if (bingoList.contains('r3')) {
       offsetY = cellHeight * 2.5;
-      drawHorizontalLine(canvas, offsetY);
+      drawHorizontalLine(canvas, offsetY, lineLength);
     }
     if (bingoList.contains('r4')) {
       offsetY = cellHeight * 3.5;
-      drawHorizontalLine(canvas, offsetY);
+      drawHorizontalLine(canvas, offsetY, lineLength);
     }
     if (bingoList.contains('r5')) {
       offsetY = cellHeight * 4.5;
-      drawHorizontalLine(canvas, offsetY);
+      drawHorizontalLine(canvas, offsetY, lineLength);
     }
     if (bingoList.contains('dRight')) {
       canvas.drawLine(
         const Offset(0, 0),
-        Offset(lineLength/5, lineLength/5),
+        Offset(lineLength, lineLength),
         _paint,
       );
     }
     if (bingoList.contains('dLeft')) {
       canvas.drawLine(
-        Offset(0, lineLength/5),
-        Offset(lineLength/5, 0),
+        Offset(0, lineLength),
+        Offset(lineLength, 0),
         _paint,
       );
     }
   }
 
-  void drawLineVerticalLine(Canvas canvas, double offsetX) {
-    double lineLength = 350.0;
+  void drawLineVerticalLine(Canvas canvas, double offsetX, double lineLength) {
     canvas.drawLine(
       Offset(offsetX, 0),
-      Offset(offsetX, lineLength + 8),
+      Offset(offsetX, lineLength),
       _paint,
     );
   }
 
-  void drawHorizontalLine(Canvas canvas, double offsetY) {
-    double lineLength = 350.0;
+  void drawHorizontalLine(Canvas canvas, double offsetY, double lineLength) {
     canvas.drawLine(
       Offset(0, offsetY),
-      Offset(lineLength + 8, offsetY),
+      Offset(lineLength , offsetY),
       _paint,
     );
   }
